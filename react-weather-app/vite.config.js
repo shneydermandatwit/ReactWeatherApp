ecdc72c18331f2fe41795a9308960c5a36164052
David Shneyderman <shneydermand@wit.edu>
2024-03-23 14:53:57 -0400

    delete

--- react-weather-app/vite.config.js
+++ react-weather-app/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Specify MIME types for specific file extensions
-    mimeTypes: {
-      '.js': 'application/javascript', // Corrected MIME type for JavaScript files
-    },
-  },
+  
 });
