61a66b50f0da012e7899cf142562ab51db9b0c43
David Shneyderman <shneydermand@wit.edu>
2024-03-23 14:39:02 -0400

    mime type

--- react-weather-app/vite.config.js
+++ react-weather-app/vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     // Specify MIME types for specific file extensions
-    // For example, to ensure that .js files are served with the correct MIME type
     mimeTypes: {
-      '.js': 'text/javascript',
+      '.js': 'application/javascript', // Corrected MIME type for JavaScript files
     },
   },
-})
+});
